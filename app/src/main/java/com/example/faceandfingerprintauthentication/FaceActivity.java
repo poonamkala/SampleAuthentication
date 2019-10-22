8b73b1d8a5f8ee1bd032a616294f5f1e2a584efe
Poonam <poonam.k@pacewisdom.in>
2019-10-22 21:30:29 +0530

    first commit

--- app/src/main/java/com/example/faceandfingerprintauthentication/FaceActivity.java
+++ app/src/main/java/com/example/faceandfingerprintauthentication/FaceActivity.java
@@ -1,16 +1,75 @@
 package com.example.faceandfingerprintauthentication;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.biometric.BiometricPrompt;
 
 
 import android.os.Bundle;
+import android.os.Handler;
+import android.widget.Toast;
+
+import java.util.concurrent.Executor;
 
 public class FaceActivity extends AppCompatActivity {
+
+    private Handler handler = new Handler();
+
+    private Executor executor = new Executor() {
+        @Override
+        public void execute(Runnable command) {
+            handler.post(command);
+        }
+    };
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_face);
+
+        BiometricPrompt.PromptInfo promptInfo = new BiometricPrompt.PromptInfo.Builder()
+                .setTitle("Authentication..")
+                .setSubtitle("Please look at front of your device to verify your identity")
+                .setNegativeButtonText("Cancel")
+                .setConfirmationRequired(false)
+                .build();
+
+        BiometricPrompt biometricPrompt = new BiometricPrompt(FaceActivity.this,
+                executor, new BiometricPrompt.AuthenticationCallback() {
+            @Override
+            public void onAuthenticationError(int errorCode,
+                                              @NonNull CharSequence errString) {
+                super.onAuthenticationError(errorCode, errString);
+                Toast.makeText(getApplicationContext(),
+                        "Authentication error: " + errString, Toast.LENGTH_SHORT)
+                        .show();
+            }
+
+            @Override
+            public void onAuthenticationSucceeded(
+                    @NonNull BiometricPrompt.AuthenticationResult result) {
+                super.onAuthenticationSucceeded(result);
+                BiometricPrompt.CryptoObject authenticatedCryptoObject =
+                        result.getCryptoObject();
+                Toast.makeText(getApplicationContext(),
+                        "Authentication success: " + result.getCryptoObject(), Toast.LENGTH_SHORT)
+                        .show();
+                // User has verified the signature, cipher, or message
+                // authentication code (MAC) associated with the crypto object,
+                // so you can use it in your app's crypto-driven workflows.
+            }
+
+            @Override
+            public void onAuthenticationFailed() {
+                super.onAuthenticationFailed();
+                Toast.makeText(getApplicationContext(), "Authentication failed",
+                        Toast.LENGTH_SHORT)
+                        .show();
+            }
+        });
+
+        // Displays the "log in" prompt.
+        biometricPrompt.authenticate(promptInfo);
 
 
     }
